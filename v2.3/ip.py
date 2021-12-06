a9bab9c543407a88a58c09d2491b87872509ce70
iicovo <iicovo@163.com>
2021-12-06 20:45:20 +0800

    默认

--- v2.3/ip.py
+++ v2.3/ip.py
@@ -61,7 +61,7 @@ async def create_aiohttp():
             task.append(get_page('http://www.nimadaili.com/http/{}/'.format(i + 1), mod=4, session=session))
             task.append(get_page('https://www.89ip.cn/index_{}.html'.format(i + 1), mod=3, session=session))
             task.append(get_page('http://http.taiyangruanjian.com/free/page{}/'.format(i + 1), mod=1, session=session))
-            task.append(get_page('https://ip.jiangxianli.com/?page={}&country=%E4%B8%AD%E5%9B%BD'.format(i + 1),
+            task.append(get_page('http://ip.jiangxianli.com/?page={}&country=%E4%B8%AD%E5%9B%BD'.format(i + 1),
                                  session=session))
             task.append(get_page('http://www.kxdaili.com/dailiip/1/{}.html'.format(i + 1), session=session))
             task.append(get_page('http://www.ip3366.net/free/?stype=1&page={}'.format(i + 1), session=session))
--- v2.3/main.py
+++ v2.3/main.py
@@ -1,14 +1,12 @@
 # encoding:utf-8
-# 利用asyncio+aiohttp实现异步访问
 import asyncio
 import aiohttp
 import ip
 
 global n  # 记录访问成功次数
 
-# 解决ssl错误
-# 参考https://www.pythonheidong.com/blog/article/506776/f797119eef523700648a/
-asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+# 可根据自己电脑更改位置
+link_path = ".\\momo_link.txt"
 
 
 # 读取文件获取文件内容
@@ -20,13 +18,12 @@ def readfile():
 
 # 读取文件获取momo链接
 def share_Link():
-    # 判断文件是否存在，不存在则创建
     import sys
     import os
+
     # 判断文件是否存在，不存在则创建
-    file = 'E:\\Desktop\\momo_link.txt'  # 可根据自己电脑更改位置
-    if os.path.isfile(file):
-        fileopen = open(file, 'r', encoding='utf-8')
+    if os.path.isfile(link_path):
+        fileopen = open(link_path, 'r', encoding='utf-8')
         momo_share_link = fileopen.readline()
         # 判断是否有链接 无则终止程序
         if momo_share_link == '':
@@ -35,8 +32,11 @@ def share_Link():
         fileopen.close()
         return momo_share_link.strip()
     else:
-        os.close(os.open(file, os.O_CREAT))  # 创建文件
+        os.close(os.open(link_path, os.O_CREAT))  # 创建文件
         sys.exit()  # 终止程序
+
+
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 
 # 实例化请求对象
@@ -45,7 +45,7 @@ async def create_aiohttp(url, proxy_list):
     global n
     n = 0
     async with aiohttp.ClientSession() as session:  # 实例化一个请求对象
-        sem = asyncio.Semaphore(50)  # 设置限制并发次数
+        sem = asyncio.Semaphore(80)  # 设置限制并发次数
         # 生成任务列表
         task = [web_request(url=url, header=header, proxy=proxy, sem=sem, session=session) for proxy in proxy_list]
         await asyncio.wait(task)
@@ -82,10 +82,11 @@ def main():
     proxies = [i.strip() for i in readfile()]  # 生成代理列表
     asyncio.run(create_aiohttp(link, proxies))  # 异步访问
     print("任务完成!!!")
-    # 桌面创建文件 呈现程序运行结果
-    # 可根据自己电脑更改位置
-    path = 'E:\\Desktop\\访问成功{}次.txt'.format(n)
-    with open(path, 'w', encoding='utf-8') as f:
+
+    # 呈现程序运行结果。可根据自己电脑更改位置
+    reminder_path = '.\\访问成功{}次.txt'.format(n)
+    # 创建运行结果提示文件
+    with open(reminder_path, 'w', encoding='utf-8') as f:
         f.close()
 
 
