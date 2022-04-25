c70e46df28fb08ab9c0abba31f1396018827d5dd
iicovo <iicovo@163.com>
2022-04-25 10:31:14 +0800

    简单调整

--- auto-momo/ip.py
+++ auto-momo/ip.py
@@ -1,8 +1,8 @@
 # encoding:utf-8
-import re
+from asyncio import create_task, wait, Semaphore, run
 from random import choice
-import asyncio
-from aiohttp import ClientSession, ClientTimeout, TCPConnector
+from aiohttp import ClientSession, ClientTimeout
+from re import findall
 
 listIP = []  # 保存IP地址
 
@@ -37,24 +37,23 @@ async def getheaders():
 # 生成任务列表
 async def taskList(ss):
     task = [
-        asyncio.create_task(get_page('http://www.kxdaili.com/dailiip/2/1.html', session=ss)),
-        get_page('https://proxy.seofangfa.com/', session=ss),
-        asyncio.create_task(get_page('https://www.kuaidaili.com/free/inha/1/', mod=2, session=ss)),
-        asyncio.create_task(
-            get_page('https://cdn.jsdelivr.net/gh/parserpp/ip_ports/proxyinfo.txt', mod=-1, session=ss)),
-        asyncio.create_task(
+        create_task(get_page('http://www.kxdaili.com/dailiip/2/1.html', session=ss)),
+        create_task(get_page('https://proxy.seofangfa.com/', session=ss)),
+        create_task(get_page('https://www.kuaidaili.com/free/inha/1/', mod=2, session=ss)),
+        create_task(get_page('https://cdn.jsdelivr.net/gh/parserpp/ip_ports/proxyinfo.txt', mod=-1, session=ss)),
+        create_task(
             get_page('https://fastly.jsdelivr.net/gh/parserpp/ip_ports@main/proxyinfo.txt', mod=-1, session=ss)),
-        asyncio.create_task(get_page('https://www.kuaidaili.com/free/intr/2/', mod=2, session=ss)),
-        asyncio.create_task(get_page('https://www.proxy-list.download/api/v1/get?type=http', mod=3, session=ss)),
+        create_task(get_page('https://www.kuaidaili.com/free/intr/2/', mod=2, session=ss)),
+        create_task(get_page('https://www.proxy-list.download/api/v1/get?type=http', mod=3, session=ss)),
     ]
 
     for i in range(1, 4):
-        task.append(asyncio.create_task(get_page(f'http://www.nimadaili.com/http/{i}/', mod=4, session=ss)))
-        task.append(asyncio.create_task(get_page(f'https://www.89ip.cn/index_{i}.html', session=ss)))
-        task.append(asyncio.create_task(get_page(f'http://http.taiyangruanjian.com/free/page{i}/', mod=1, session=ss)))
-        task.append(asyncio.create_task(get_page(f'http://www.kxdaili.com/dailiip/1/{i}.html', session=ss)))
-        task.append(asyncio.create_task(get_page(f'http://www.ip3366.net/free/?stype=1&page={i}', session=ss)))
-        task.append(asyncio.create_task(get_page(f'https://www.dieniao.com/FreeProxy/{i}.html', mod=5, session=ss)))
+        task.append(create_task(get_page(f'http://www.nimadaili.com/http/{i}/', mod=4, session=ss)))
+        task.append(create_task(get_page(f'https://www.89ip.cn/index_{i}.html', session=ss)))
+        task.append(create_task(get_page(f'http://http.taiyangruanjian.com/free/page{i}/', mod=1, session=ss)))
+        task.append(create_task(get_page(f'http://www.kxdaili.com/dailiip/1/{i}.html', session=ss)))
+        task.append(create_task(get_page(f'http://www.ip3366.net/free/?stype=1&page={i}', session=ss)))
+        task.append(create_task(get_page(f'https://www.dieniao.com/FreeProxy/{i}.html', mod=5, session=ss)))
     return task
 
 
@@ -62,15 +61,15 @@ async def taskList(ss):
 async def create_aiohttp_ip():
     async with ClientSession() as session:
         task = await taskList(session)
-        await asyncio.wait(task)
+        await wait(task)
 
 
 # 访问网页
 async def get_page(url, session, mod=0):
-    tout = ClientTimeout(total=20)
+    tout = ClientTimeout(total=5)
     hd = await getheaders()
     try:
-        async with asyncio.Semaphore(3):
+        async with Semaphore(3):
             async with await session.get(url=url, headers=hd, timeout=tout) as response:
                 page_source = await response.text()
                 await soup_page(page_source, mod=mod)
@@ -81,8 +80,8 @@ async def get_page(url, session, mod=0):
 async def soup_page(source, mod):
     if mod == 0:
         # 通用
-        ips = re.findall(r'<td>[\s]*?(\d+\.\d+\.\d+\.\d+)[\s]*?</td>', source)
-        posts = re.findall(r'<td>[\s]*?(\d{1,5})[\s]*?</td>', source)
+        ips = findall(r'<td>[\s]*?(\d+\.\d+\.\d+\.\d+)[\s]*?</td>', source)
+        posts = findall(r'<td>[\s]*?(\d{1,5})[\s]*?</td>', source)
         for i in range(len(ips)):
             listIP.append(f"http://{ips[i]}:{posts[i]}")
 
@@ -93,15 +92,15 @@ async def soup_page(source, mod):
 
     elif mod == 1:
         # 太阳
-        ips = re.findall(r'<div.*?">(\d+\.\d+\.\d+\.\d+)</div>', source)
-        posts = re.findall(r'<div.*?">(\d{1,5})</div>', source)
+        ips = findall(r'<div.*?">(\d+\.\d+\.\d+\.\d+)</div>', source)
+        posts = findall(r'<div.*?">(\d{1,5})</div>', source)
         for i in range(len(ips)):
             listIP.append(f"http://{ips[i]}:{posts[i]}")
 
     elif mod == 2:
         # 快代理
-        ips = re.findall(r'<td\s.*?="IP">(\d+\.\d+\.\d+\.\d+)</td>', source)
-        posts = re.findall(r'<td\s.*?="PORT">(\d{1,5})</td>', source)
+        ips = findall(r'<td\s.*?="IP">(\d+\.\d+\.\d+\.\d+)</td>', source)
+        posts = findall(r'<td\s.*?="PORT">(\d{1,5})</td>', source)
         for i in range(len(ips)):
             listIP.append(f"http://{ips[i]}:{posts[i]}")
 
@@ -113,14 +112,14 @@ async def soup_page(source, mod):
 
     elif mod == 4:
         # 泥马代理
-        ip_post = re.findall(r'<td>(.*?:\d+)</td>', source)
+        ip_post = findall(r'<td>(.*?:\d+)</td>', source)
         for i in ip_post:
             listIP.append(f"http://{i}")
 
     elif mod == 5:
         # 蝶鸟
-        ip_list = re.findall(r"<span\sclass='f-address'>(.*?)</span>", source)[1:]
-        port_list = re.findall(r"<span class='f-port'>(\d+)</span>", source)
+        ip_list = findall(r"<span\sclass='f-address'>(.*?)</span>", source)[1:]
+        port_list = findall(r"<span class='f-port'>(\d+)</span>", source)
         for i in range(len(ip_list)):
             listIP.append(f'http://{ip_list[i]}:{port_list[i]}')
 
@@ -128,14 +127,14 @@ async def soup_page(source, mod):
         # 站大爷
         pass
     elif mod == 7:
-        ips = re.findall(r'<td>.*?(\d+\.\d+\.\d+\.\d+)</td>', source)
-        posts = re.findall(r'<td>(\d{1,5})</td>', source)
+        ips = findall(r'<td>.*?(\d+\.\d+\.\d+\.\d+)</td>', source)
+        posts = findall(r'<td>(\d{1,5})</td>', source)
         for i in range(len(ips)):
             listIP.append(f'http://{ips[i]}:{posts[i]}')
 
 
 def ip_main():
-    asyncio.run(create_aiohttp_ip())
+    run(create_aiohttp_ip())
     global listIP
     listIP = list(set(listIP))  # 代理去重
     print(f"代理ip抓取完成,共{len(listIP)}个可用代理ip地址。")
--- auto-momo/momo.py
+++ auto-momo/momo.py
@@ -1,7 +1,7 @@
 # encoding:utf-8
 from os import environ
 from ip import listIP, getheaders, ip_main
-import asyncio
+from asyncio import create_task, wait, Semaphore, run
 from aiohttp import ClientSession, ClientTimeout
 
 global n  # 记录访问成功次数
@@ -17,18 +17,17 @@ async def create_aiohttp(url, proxy_list):
     n = 0
     async with ClientSession() as session:
         # 生成任务列表
-        task = [asyncio.create_task(web_request(url=url, proxy=proxy, session=session)) for
-                proxy in proxy_list]
-        await asyncio.wait(task)
+        task = [create_task(web_request(url, proxy, session)) for proxy in proxy_list]
+        await wait(task)
 
 
 # 网页访问
 async def web_request(url, proxy, session):
     # 并发限制
-    async with asyncio.Semaphore(3):
+    async with Semaphore(5):
         try:
             async with await session.get(url=url, headers=await getheaders(), proxy=proxy,
-                                         timeout=ClientTimeout(total=20)) as response:
+                                         timeout=ClientTimeout(total=10)) as response:
                 # 返回字符串形式的相应数据
                 page_source = await response.text()
                 await page(page_source)
@@ -45,7 +44,7 @@ async def page(page_source):
 
 def main():
     ip_main()  # 抓取代理
-    asyncio.run(create_aiohttp(link, listIP))
+    run(create_aiohttp(link, listIP))
     print(f"墨墨分享链接访问成功{n}次。")
 
 
